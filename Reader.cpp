449cf2ef634412f43b8f78599e7d2abb58ab8817
fllamas <chewfasa5@hotmail.com>
2023-02-08 16:30:17 -0800

    Fixed repeating code and cleaned up code style in Reader.cpp

--- Reader.cpp
+++ Reader.cpp
@@ -1,12 +1,7 @@
 #include "reader.h"
 
-#include "reader.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-
-void Reader::getNames() {
+void Reader::getNames() 
+{
   std::string name;
 
   if (fileName.is_open()) {
@@ -16,24 +11,4 @@ void Reader::getNames() {
   } else {
     std::cerr << "Unable to open the file." << std::endl;
   }
-}
-
-void Reader::print() {
-  std::cout << "Group 1 8 members:" << std::endl;
-
-  for (auto name : names) {
-    std::cout << name << std::endl;
-  }
-}
-
-int main() {
-  std::string fileName;
-  std::cout << "Enter the name of the file: ";
-  std::cin >> fileName;
-
-  Reader reader(fileName);
-  reader.getNames();
-  reader.print();
-
-  return 0;
 }
